c2e263e05962f546e84850b9c9111234cb6f9907
KMkumada <s1321155@cco.kanagawa-it.ac.jp>
2016-12-31 15:15:00 +0900

    elasticsearch処理部分

--- new_ela.php
+++ new_ela.php
@@ -1,9 +1,11 @@
 <?php
-//  if ($_POST) {
-/*      $replyData = $_POST['content'];
+//入力フォームからは受け取れています。確認用にコメントアウトしてます
+ /*if ($_POST) {
+      $replyData = $_POST['content'];
+*/
 
-      $replyData = 'POST確認-->'.$replyData;
-      */
+//必ずelasticsearchの起動を確認してください
+//elasticsearch起動する記述がありませんでした12/31
       require 'vendor/autoload.php';
       use Elasticsearch\ClientBuilder;
       //クラスの初期化
@@ -15,6 +17,7 @@
               'query' => [
                   'match' => [
                       'description' =>'渋め'
+                      //変数でにする予定です12/31
                   ]
               ]
           ]
